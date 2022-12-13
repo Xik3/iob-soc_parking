a8f9f2412d6c4b4beb2cc5efbc2388cd85933553
Francisco Duarte <88357917+Xik3@users.noreply.github.com>
2022-12-13 23:16:56 +0000

    Update system_top_core.v

--- hardware/simulation/verilog_tb/system_top_core.v
+++ hardware/simulation/verilog_tb/system_top_core.v
@@ -89,7 +89,14 @@ module system_top
       .rst(rst)
       );   
 `endif
-
+  reg [32-1:0] ginput = 0;
+  assign gpio_input = ginput;
+  
+  initial begin
+  
+    #45 ginput = 1;
+ 
+  end
    
    //finish simulation on trap
    /* always @(posedge trap) begin
